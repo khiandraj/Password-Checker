1fe92d78d17b3c8f9ece00684d67346487175e9f
khiandraj <kjoh6987@students.vsu.edu>
2025-08-27 15:39:13 -0400

    Initial Commit

---
+++ PasswordChecker.Tests/PasswordCheck.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using PasswordCheck;
+
+
+namespace PasswordCheckerTests;
+
+
+public class PasswordCheckTests
+{
+        [Fact]
+        public void NoCriteriaMet()
+        {
+            Assert.True("INELLIGIBLE", Strength.PasswordCheck(""));
+            Assert.True("INELLIGIBLE", Strength.PasswordCheck("  "));
+
+        }
+        [Fact]
+        public void OneCriteriaMet()
+        {
+            Assert.True("WEAK", Strength.PasswordCheck("abcdef"));
+            Assert.True("WEAK", Strength.PasswordCheck("ABCDF"));
+            Assert.True("WEAK", Strength.PasswordCheck("123456"));
+            Assert.True("WEAK", Strength.PasswordCheck("!!!!!"));
+        }
+        [Fact]
+        public void TwoOrThreeCriteriaMet()
+        {
+            Assert.True("MEDIUM", Strength.PasswordCheck("abc123"));
+            Assert.True("MEDIUM", Strength.PasswordCheck("Abc123"));
+            Assert.True("MEDIUM", Strength.PasswordCheck("ABCabc"));
+
+        }
+        [Fact]
+        public void AllCriteriaMet()
+        {
+            Assert.True("STRONG", Strength.PasswordCheck("Abc123!"));
+            Assert.True("STRONG", Strength.PasswordCheck("VsU2027$!"));
+        }
+
+    }
+    
+
+
+    
+
---
+++ PasswordChecker/Strength.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace PasswordChecker;
+
+
+public class Class1
+{
+    public static string CheckStrength(string password)
+    {
+
+
+    bool hasUpper = false;
+    bool hasLower = false; 
+    bool hasDigit = false; 
+    bool hasSymbol = false; 
+
+    foreach (char ch in password)
+    {
+        if (char.IsUpper(ch))
+            hasUpper = true;
+        else if (char.IsLower(ch))
+            hasLower = true;
+        else if (char.IsDigit(ch))
+            hasDigit = true;
+        else
+            hasSymbol = true;
+    }
+
+    int metCriteria = 0;
+    if (hasUpper) metCriteria++;
+    if (hasLower) metCriteria++;
+    if (hasDigit) metCriteria++;
+    if (hasSymbol) metCriteria++;
+
+    if (metCriteria == 0)
+        return "INELIGIBLE";
+    else if (metCriteria == 1)
+        return "WEAK";
+    else if (metCriteria == 2 || metCriteria == 3)
+        return "MEDIUM";
+    else if (metCriteria == 4)
+        return "STRONG"; 
+    else
+        return "INELIGIBLE"; 
+
+
+
+
+    
+        
+
+        
+
+
+        
+
+}
+}
